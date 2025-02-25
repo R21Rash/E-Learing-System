986f1ce9f433af8b0170f48df6fe1ba65abc7240
R21Rash <153453806+R21Rash@users.noreply.github.com>
2025-02-25 22:46:59 +0530

    initial commit by rashad

---
+++ com.providers/src/com/providers/Activator.java
@@ -0,0 +1,22 @@
+package com.providers;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		System.out.println("this is rashad testing");
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("this is stopped");
+	}
+
+}
